7c907de4461566d2fb38dff8abd20802db48bcb7
Austin <austinsusanto@gmail.com>
2024-04-13 13:59:14 +0700

    (3) Validating request and selectively responding

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # REFLECTION
 
+## Commit 1
+
 From the tutorial, we can conclude that Rust's is a complicated programming language with a beginner unfriendly syntax. But, it is also a language that is capable of doing what other language are capable of.
 
+## Commit 2
+
 ![Commit 2 screen capture](/assets/images/commit2.png)
+
+## Commit 3
+
+![Commit 3 screen capture](/assets/images/commit3.png)
--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,27 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream); 
     let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
-    let status_line = "HTTP/1.1 200 OK"; 
-    let contents = fs::read_to_string("hello.html").unwrap(); 
+
+    let path = if let Some(first_line) = http_request.first() {
+        let mut parts = first_line.split_whitespace();
+
+        if let Some(path) = parts.nth(1) {
+            path
+        } else {
+            "/"
+        }
+    } else {
+        "/"
+    };
+
+    let (status_line, contents) = if path == "/" {
+        ("HTTP/1.1 200 OK", fs::read_to_string("hello.html").unwrap())
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", fs::read_to_string("bad.html").unwrap())
+    };
+    
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
     
     stream.write_all(response.as_bytes()).unwrap();
-   }
+}
